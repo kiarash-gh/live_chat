e59f5cc8b67daff705e3c9744152399ecb6744e0
kiarash-gh <kiarash.gh@gmail.com>
2025-06-27 11:55:40 +0330

    init: start live chat project

---
+++ live_chat/chat/consumers.py
@@ -0,0 +1,27 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add("chat", self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard("chat", self.channel_name)
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.channel_layer.group_send(
+            "chat",
+            {
+                "type": "chat_message",
+                "username": data["username"],
+                "message": data["message"],
+            }
+        )
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
+            "username": event["username"],
+            "message": event["message"],
+        }))
